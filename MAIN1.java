de35c354f9af1e148e013947dd12693b54087116
RealMentalMan <144133729+RealMentalMan@users.noreply.github.com>
2023-09-07 19:20:15 +0300

    Update MAIN1.java

--- MAIN1.java
+++ MAIN1.java
@@ -9,47 +9,34 @@ class VALUTI {
         Dkurse = Dollar_kurse;
         Ekurse = Euro_kurse;
     }
-
-
     public double Dkonvert(double i) {
         double res = 0;
-        if (way == 1)
-            res = Dkurse * i;
+        if (way == 1) res = Dkurse * i;
         if (way == 0)
             res = i / Dkurse;
 
         return res;
     }
-
     public double Ekonvert(double i) {
         double res = 0;
         if (way == 1)
             res = Ekurse * i;
         if (way == 0)
             res = i / Ekurse;
-
         return res;
     }
-
-
     public double get_D() {
         return Dkurse;
     }
-
     public double get_E() {
         return Ekurse;
     }
-
-
     public void set_D(double i) {
         Dkurse = i;
     }
-
     public void set_E(double i) {
         Ekurse = i;
     }
-
-
 }
 public class MAIN1 {
     public static void main(String[] args) {
@@ -61,9 +48,9 @@ public class MAIN1 {
         NumberFormat NUM2 = NumberFormat.getCurrencyInstance(Locale.FRANCE);
         System.out.println("Я продал " + NUM.format(MyDollars) + " и получил " + MyRubles + " руб.");
         double LOP = 1;
-        System.out.println(V.get_E() +  " руб. = " +  NUM2.format(LOP));
+        System.out.println(NUM2.format(LOP) + " = " + V.get_E() + " руб."  );
         V.set_E(111.11);
-        System.out.println("Теперь "  + V.get_E() +  " руб. = " +  NUM2.format(LOP));
+        System.out.println("Теперь "  + NUM2.format(LOP) + " = " + V.get_E() + " руб." );
     }
 }
 
